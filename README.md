9b6acf9c6645b8028f329ec4dd334b640902396a
arwinlashawn <aanakkendary@ucdavis.edu>
2020-08-28 10:38:14 +0800

    added first project and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # opencv_projects
 All mini projects from an OpenCV course.
+
+## Project 1: Live Pencil Sketch App 
+* Edge detection algorithm used: Canny
+> Preview
+> ![](https://i.imgur.com/VkFQN0W.png)
---
+++ live_pencil_sketcher_project.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np 
+import os 
+
+os.chdir("/Users/macintosh/Desktop")
+
+### Live Pencil Sketch App! 
+# for some reason this only works in Jupyter Notebook, not Sublime Text
+
+def sketch(image):
+	# first convert image to grayscale
+	img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# clean up image using Gaussian Blue (removes noise)
+	img_gray_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+
+	# time to extract edges using the best algo, canny
+	canny_edges = cv2.Canny(img_gray_blur, 10, 70)
+
+	# need to invert binarize the image. if not done, it will be a black backrground with white edges 
+	ret, mask = cv2.threshold(canny_edges, 70, 255, cv2.THRESH_BINARY_INV)
+	return mask
+
+
+# initialize webcam, cap is the project provided by VideoCapture
+# it contains a boolean indicating if it was successful or not (ret)
+# it also contains the images collected from the webcam (frame)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read()
+	cv2.imshow("My Live Sketcher", sketch(frame))
+	if cv2.waitKey(1) == 13: # 13 here is the Enter key
+		break
+
+# to release camera and close windows
+cap.release()
+cv2.destroyAllWindows()
+
+
